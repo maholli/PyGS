197b070efca6bff42f3680c0aec3557a2bab0e38
Max <H>
2021-03-04 17:17:15 -0800

    overhaul for FeatherS2 instead of SAM32

---
+++ code/code.py
@@ -0,0 +1,125 @@
+import wifi, socketpool, time, alarm
+import adafruit_minimqtt.adafruit_minimqtt as MQTT
+from radio_helpers import gs, mqtt_message, connected
+from secrets import secrets
+import storage,os
+
+DEEP_SLEEP_TIME = 30
+DATA_TOPIC = 'gs/testsub'
+CTRL_TOPIC = 'gs/remote'
+
+radios=[]
+# if we haven't slept yet, init radios
+if not alarm.wake_alarm:
+    print('First boot')
+    radios = gs.init_radios(gs.SATELLITE['VR3X'])
+    # reset counters
+    gs.counter = 0
+    gs.msg_count = 0
+    gs.msg_cache = 0
+    gs.cache_pointer = gs.CACHE_START
+else:
+    radios={1:gs.R1_CS,2:gs.R2_CS,3:gs.R3_CS}
+
+
+print('Loop: {}, Total Msgs: {}, Msgs in Cache: {}'.format(gs.counter,gs.msg_count,gs.msg_cache))
+
+# try connecting to wifi
+print('Connecting to WiFi...')
+try:
+    # wifi.radio.connect(ssid='W6YX') # open network
+    wifi.radio.connect(ssid='S8hotspot') # open network
+
+    # Create a socket pool
+    pool = socketpool.SocketPool(wifi.radio)
+    # sync out RTC from the web
+    gs.synctime(pool)
+except Exception as e:
+    print('Unable to connect to WiFi: {}'.format(e))
+
+# check radios
+new_messages=[]
+if alarm.wake_alarm:
+    for r in radios:
+        if gs.rx_done(radios[r]):
+            gs.rgb[0]=(0,255,0)
+            print(r,end=': ')
+            for msg in gs.get_msg2(radios[r]):
+                if msg is not None:
+                    print(bytes(msg), end=', ')
+                    if msg is b'CRC ERROR':
+                        msg_str = '{},{},{}'.format(time.time(),r,'CRC ERROR')
+                        new_messages.append(msg_str)
+                    else:
+                        msg_str = '{},{},{}'.format(time.time(),r,bytes(msg))
+                        new_messages.append(msg_str)
+            print()
+    radios = gs.init_radios(gs.SATELLITE['VR3X'])
+else:
+    for r in radios:
+        if r.rx_done():
+            gs.rgb[0]=(0,255,0)
+            print(r.name,end=': ')
+            for msg in gs.get_msg(r):
+                if msg is not None:
+                    print(bytes(msg), end=', ')
+                    if msg is b'CRC ERROR':
+                        msg_str = '{},{},{}'.format(time.time(),r.name,'CRC ERROR')
+                        new_messages.append(msg_str)
+                    else:
+                        msg_str = '{},{},{}'.format(time.time(),r.name,bytes(msg))
+                        new_messages.append(msg_str)
+            print()
+
+if new_messages:
+    gs.msg_count = gs.msg_count + 1
+
+# if we have wifi, connect to mqtt broker
+if wifi.radio.ap_info is not None:
+    # try:
+    # Set up a MiniMQTT Client
+    mqtt_client = MQTT.MQTT(
+        broker=secrets['broker'],
+        port=secrets['port'],
+        socket_pool=pool,
+        is_ssl=False,
+    )
+    mqtt_client.on_connect = connected
+    mqtt_client.on_message = mqtt_message
+
+    mqtt_client.connect()
+    mqtt_client.subscribe(CTRL_TOPIC)
+    mqtt_client.publish(DATA_TOPIC,'[{}][{}] Loop:{}, Msg Cnt:{}, Msg Cache:{}'.format(time.time(),gs.myuid,gs.counter,gs.msg_count,gs.msg_cache))
+
+    # send any cached messages
+    if gs.msg_cache:
+        with open('/data.txt','r') as f:
+            l=f.readline()
+            while l:
+                mqtt_client.publish(DATA_TOPIC,'[cached][{}] {}'.format(gs.myuid,l.strip()))
+                l=f.readline()
+        os.remove('/data.txt')
+        gs.msg_cache=0
+
+    # send any new messages
+    if new_messages:
+        for msg in new_messages:
+            mqtt_client.publish(DATA_TOPIC,'[new] {}'.format(msg))
+    mqtt_client.disconnect()
+
+# if we can't connect, cache message
+else:
+    for msg in new_messages:
+        try:
+            storage.remount('/',False)
+            with open('/data.txt','a') as f:
+                f.write(msg+'\n')
+            storage.remount('/',True)
+        except: pass
+        gs.msg_cache=gs.msg_cache+1
+
+
+print('Finished. Deep sleep for {} seconds.'.format(DEEP_SLEEP_TIME))
+gs.counter = gs.counter + 1
+time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + DEEP_SLEEP_TIME)
+alarm.exit_and_deep_sleep_until_alarms(time_alarm)
---
+++ code/radio_helpers.py
@@ -0,0 +1,250 @@
+import digitalio,board, time, alarm
+import pycubed_rfm9x
+import adafruit_requests,rtc,json
+from microcontroller import cpu
+import adafruit_dotstar
+
+FIFO = bytearray(256)
+fifo_view = memoryview(FIFO)
+
+def mqtt_message(client, feed_id, payload):
+    print("[{0}] {1}".format(feed_id, payload))
+    try:
+        if payload[:2]=='EV':
+            client.publish('gs/control/response',str(eval(payload[2:])))
+        elif payload[:2]=='EX':
+            exec(payload[2:].encode())
+        elif payload[:4]=='PING':
+            client.publish('gs/control/response',str(sam32.battery_voltage()))
+    except Exception as e:
+        print('error: {}'.format(e))
+        print(type(payload))
+        client.publish('gs/control/response',str(e))
+
+def connected(client, userdata, flags, rc):
+    # This function will be called when the client is connected
+    # successfully to the broker.
+    print("Connected to MQTT broker!")
+    # Subscribe to all changes on the default_topic feed.
+    client.subscribe('gs/testsub')
+
+class GroundStation:
+    CACHE_START = 7
+    myuid=int.from_bytes(cpu.uid,'big')
+
+    SATELLITE = {
+        'NORBI':{'NAME':'NORBI','FREQ':436.703,'SF':10,'BW':250000,'CR':8},
+        'VR3X' :{'NAME':'VR3X','FREQ':915.6,'SF':7,'BW':62500,'CR':8}
+    }
+
+    def __init__(self):
+        LED= digitalio.DigitalInOut(board.LED)
+        LED.switch_to_output(True)
+        self.rgb = adafruit_dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.5, auto_write=True)
+        self.rgb[0]=(0,0,255)
+        self.spi=board.SPI()
+        self.R1_CS  = digitalio.DigitalInOut(board.D5)
+        self.R1_CS.switch_to_output(True)
+        self.R2_CS  = digitalio.DigitalInOut(board.D20)
+        self.R2_CS.switch_to_output(True)
+        self.R3_CS  = digitalio.DigitalInOut(board.D12)
+        self.R3_CS.switch_to_output(True)
+        self._BUFFER=bytearray(256)
+
+
+
+    def init_radios(self,config):
+        # define radio pins
+        # 1 - RST:B(D61/D6) CS:C(DAC0/D5)
+        R1_RST = digitalio.DigitalInOut(board.D6)
+        R1_RST.switch_to_output(True)
+        # 2 - RST:D(A7/D21)  CS:E(A8/D20)
+        R2_RST = digitalio.DigitalInOut(board.D21)
+        R2_RST.switch_to_output(True)
+        # 3 - RST:D59/D12    CS:DAC1/D13
+        R3_RST = digitalio.DigitalInOut(board.D13)
+        R3_RST.switch_to_output(True)
+
+        # initialize radios
+        radio1 = pycubed_rfm9x.RFM9x(board.SPI(),self.R1_CS,R1_RST,config['FREQ'],code_rate=config['CR'],baudrate=1320000)
+        radio2 = pycubed_rfm9x.RFM9x(board.SPI(),self.R2_CS,R2_RST,config['FREQ'],code_rate=config['CR'],baudrate=1320000)
+        radio3 = pycubed_rfm9x.RFM9x(board.SPI(),self.R3_CS,R3_RST,config['FREQ'],code_rate=config['CR'],baudrate=1320000)
+        radio1.name=1
+        radio2.name=2
+        radio3.name=3
+        # configure radios
+        for r in (radio1,radio2,radio3):
+            r.node = 0x33 # ground station ID
+            r.idle()
+            r.spreading_factor=config['SF']
+            r.signal_bandwidth=config['BW']
+            r.coding_rate=config['CR']
+            r.preamble_length = 8
+            r.enable_crc=True
+            r.low_datarate_optimize = True
+            r.ack_wait=2
+            r.ack_delay=0.2
+            r.ack_retries=0
+            r.listen()
+        return (radio1,radio2,radio3)
+
+
+    def synctime(self,pool):
+        try:
+            requests = adafruit_requests.Session(pool)
+            TIME_API = "http://worldtimeapi.org/api/ip"
+            the_rtc = rtc.RTC()
+            response = None
+            while True:
+                try:
+                    print("Fetching json from", TIME_API)
+                    response = requests.get(TIME_API)
+                    break
+                except (ValueError, RuntimeError) as e:
+                    print("Failed to get data, retrying\n", e)
+                    continue
+
+            json1 = response.json()
+            # print(json1)
+            current_time = json1['datetime']
+            the_date, the_time = current_time.split('T')
+            year, month, mday = [int(x) for x in the_date.split('-')]
+            the_time = the_time.split('.')[0]
+            hours, minutes, seconds = [int(x) for x in the_time.split(':')]
+
+            # We can also fill in these extra nice things
+            year_day = json1['day_of_year']
+            week_day = json1['day_of_week']
+            is_dst = json1['dst']
+
+            now = time.struct_time((year, month, mday, hours, minutes, seconds, week_day, year_day, is_dst))
+            the_rtc.datetime = now
+        except Exception as e:
+            print('[WARNING]',e)
+
+    @property
+    def counter(self):
+        return int.from_bytes(alarm.sleep_memory[0:2],'big')
+    @counter.setter
+    def counter(self,value):
+        alarm.sleep_memory[0:2] = int(value % 65536).to_bytes(2,'big')
+
+    @property
+    def msg_count(self):
+        return int.from_bytes(alarm.sleep_memory[2:4],'big')
+    @msg_count.setter
+    def msg_count(self,value):
+        alarm.sleep_memory[2:4] = int(value % 65536).to_bytes(2,'big')
+
+    @property
+    def msg_cache(self):
+        return alarm.sleep_memory[4]
+    @msg_cache.setter
+    def msg_cache(self,value):
+        alarm.sleep_memory[4] = value
+
+    @property
+    def cache_pointer(self):
+        return int.from_bytes(alarm.sleep_memory[5:6],'big')
+    @cache_pointer.setter
+    def cache_pointer(self,value):
+        alarm.sleep_memory[5:7] = int(value % 65536).to_bytes(2,'big')
+
+    def cache(self,msg):
+        cache_pointer = self.cache_pointer
+        msg_encoded = msg.encode()
+        msg_length = len(msg_encoded)
+        alarm.sleep_memory[cache_pointer:cache_pointer+msg_length] = msg_encoded
+        self.cache_pointer = cache_pointer+msg_length
+        self.msg_cache+=1
+
+    def _read_into(self, radio_cs, address, buf, length=None):
+        if length is None:
+            length = len(buf)
+        self._BUFFER[0] = address & 0x7F  # Strip out top bit to set read value
+        radio_cs.value=False
+        self.spi.try_lock()
+        self.spi.write(bytes([(address & 0x7F)]))
+        self.spi.readinto(buf,end=length)
+        radio_cs.value=True
+        self.spi.unlock()
+        # print(buf)
+
+    def _read_u8(self,radio_cs,address):
+        self._read_into(radio_cs,address,self._BUFFER,1)
+        return self._BUFFER[0]
+
+    def _write_u8(self, radio_cs, address, val):
+        radio_cs.value=False
+        self.spi.try_lock()
+        self._BUFFER[0] = (address | 0x80) & 0xFF  # Set top bit to 1
+        self._BUFFER[1] = val & 0xFF
+        self.spi.write(self._BUFFER, end=2)
+        radio_cs.value=True
+        self.spi.unlock()
+
+    def rx_done(self,radio_cs):
+        return (self._read_u8(radio_cs,0x12) & 0x40) >> 6
+
+    def get_msg2(self,radio_cs):
+        # hacky way of reading radio RX buffer without reinitalizing the radios
+        if not (self._read_u8(radio_cs,0x12) & 0x40) >> 6:
+            pass
+        else:
+            packet=None
+            error=1
+            # put into idle mode
+            reg=self._read_u8(radio_cs,0x01)
+            reg&=~7 # mask
+            reg|= (1&0xFF) # standby
+            self._write_u8(radio_cs,0x01,reg)
+            if not (self._read_u8(radio_cs,0x12) & 0x20) >> 5:
+                l=self._read_u8(radio_cs,0x13) # fifo length
+                # print(l)
+                if l:
+                    pos = self._read_u8(radio_cs,0x10)
+                    self._write_u8(radio_cs,0x0D, pos)
+                    packet = fifo_view[:l]
+                    self._read_into(radio_cs,0,packet)
+                error=0
+            else:
+                print('crc error')
+                yield b'CRC ERROR'
+            # clear IRQ flags
+            self._write_u8(radio_cs,0x12, 0xFF)
+            # start listening again
+            reg=self._read_u8(radio_cs,0x01)
+            reg&=~7 # mask
+            reg|= (5&0xFF) # RX mode
+            self._write_u8(radio_cs,0x01,reg)
+            yield packet
+
+    def get_msg(self,r):
+        tout = time.monotonic()+2
+        while time.monotonic() < tout:
+            if not r.rx_done():
+                pass
+            else:
+                packet=None
+                error=1
+                r.idle()
+                if not r.crc_error():
+                    l=r._read_u8(0x13) # fifo length
+                    # print(l)
+                    if l:
+                        pos = r._read_u8(0x10)
+                        r._write_u8(0x0D, pos)
+                        packet = fifo_view[:l]
+                        r._read_into(0,packet)
+                    error=0
+                else:
+                    print('crc error')
+                    yield b'CRC ERROR'
+                # clear IRQ flags
+                r._write_u8(0x12, 0xFF)
+                # start listening again
+                r.operation_mode = 5
+                tout = time.monotonic() + 2
+                yield packet
+
+gs = GroundStation()
